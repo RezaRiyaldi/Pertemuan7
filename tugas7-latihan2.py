61754fcb7ba62b6143ee876424127a8c5cf5255d
RezaRiyaldi <52364173+RezaRiyaldi@users.noreply.github.com>
2020-11-04 13:26:43 +0700

    Update tugas7-latihan2.py

--- tugas7-latihan2.py
+++ tugas7-latihan2.py
@@ -2,7 +2,7 @@ import random
 print(39*"=")
 print("Bilangan acak yang lebih kecil dari 0,5")
 print(39*"=")
-jum = int( input("Masukan nilai: "))
+jum = int( input("Masukan nilai n : "))
 i = 0
 for i in range(jum):
     i += 1
